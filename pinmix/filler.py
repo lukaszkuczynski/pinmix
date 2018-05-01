b63e07a77e991e248747a9f82e4ac7ffc28e9265
lukaszkuczynski <kuczynskilukasz@gmail.com>
2018-05-01 19:21:30 +0200

    Can save as ODT file

--- pinmix/filler.py
+++ pinmix/filler.py
@@ -2,6 +2,7 @@ import jieba
 from xpinyin import Pinyin
 import re
 import logging
+from pinmix.opendoc_factory import DocFactory
 
 def line_to_pinyin(line):
     jieba.setLogLevel(logging.ERROR)
@@ -15,14 +16,31 @@ def is_chinese(line):
     matches = re.findall(r'[\u4e00-\u9fff]+', line)
     return len(matches) > 0
 
-def process_file(filename):
+def gen_line_from_file(filename):
     with open(filename) as f:
         lines = f.readlines()
         for line in lines:
             line = line.strip()
             if is_chinese(line):
-                print(line)
-                print(line_to_pinyin(line))
+                yield ('chinese', line)
+                yield ('pinyin', line_to_pinyin(line))
             else:
-                print(line)
+                yield ('other', line)
 
+
+def process_to_print(filename):
+    for tuple in gen_line_from_file(filename):
+        print(tuple[1])
+
+
+def process_file_to_odt(filename, outputdoc):
+    doc = DocFactory(outputdoc)
+    for tuple in gen_line_from_file(filename):
+        text = tuple[1]
+        if tuple[0] == 'other':
+            doc.add_p1_text(text)
+        if tuple[0] == 'chinese':
+            doc.add_p3_text(text)
+        if tuple[0] == 'pinyin':
+            doc.add_p2_text(text)
+    doc.save()
--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
         name='pinmix',
         version='0.1',
         packages=['pinmix'],
-        install_requires=['jieba','xpinyin'],
+        install_requires=['jieba','xpinyin','odfpy'],
         entry_points={
             'console_scripts':['pinmix=pinmix.cmdline:main']
         }
