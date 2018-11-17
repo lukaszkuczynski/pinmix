763c72e2327f8de073612e8c5a4997fff8c5d238
kuczynskilukasz <kuczynskilukasz@gmail.com>
2018-11-17 10:23:55 +0100

    Fixed broken API for newer version of xpinyin, version specified

--- setup.py
+++ setup.py
@@ -4,7 +4,11 @@ setup(
         name='pinmix',
         version='0.1',
         packages=['pinmix'],
-        install_requires=['jieba','xpinyin','odfpy'],
+        install_requires=[
+		'jieba',
+		'xpinyin==0.5.5',
+		'odfpy'
+	],
         entry_points={
             'console_scripts':['pinmix=pinmix.cmdline:main']
         }
